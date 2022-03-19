efbfdd9b14f7745e774a36a3f53dbe1ae2343d3d
Kevin <psikeo@googlemail.com>
2022-03-19 14:41:07 +0100

    initial commit; adapter template

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'ir_remote_input adapter settings': {
+        'en': 'Adapter settings for ir_remote_input',
+        'de': 'Adaptereinstellungen für ir_remote_input',
+        'ru': 'Настройки адаптера для ir_remote_input',
+        'pt': 'Configurações do adaptador para ir_remote_input',
+        'nl': 'Adapterinstellingen voor ir_remote_input',
+        'fr': "Paramètres d'adaptateur pour ir_remote_input",
+        'it': "Impostazioni dell'adattatore per ir_remote_input",
+        'es': 'Ajustes del adaptador para ir_remote_input',
+        'pl': 'Ustawienia adaptera dla ir_remote_input',
+        'zh-cn': 'ir_remote_input的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
